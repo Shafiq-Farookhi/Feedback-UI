3471630b401fe2a67554e8f1fe0cec4dcdcb43db
Shafiq <Shahramfarokhi421@gmail.com>
2024-12-30 14:33:59 +0430

    rating and submit form updated

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,27 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet</p>
     }
-  return (
-    <div className="feedback-list">
-        {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-        ))}
-    </div>
-  )
+
+    return (
+      <div className="feedback-list">
+          {feedback.map((item) => (
+              <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+          ))}
+      </div>
+
+    )
+
+  // return (
+  //   <div className="feedback-list">
+  //       {feedback.map((item) => (
+  //           <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+  //       ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList
